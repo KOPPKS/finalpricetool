7c438b7d373c1e5cfcb805082bee3c1e4ef40258
Kevin Kopp <koppks323@yahoo.com>
2015-11-19 17:17:02 -0700

    More CSS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('ModOne', ['firebase', 'ui.router', 'ui.bootstrap']);
 
-app.controller('mainCtrl', function($scope) {
-	$scope.name = "JMoney";
-	$scope.friends = ['Ron', 'Bob', 'Wes'];
-});
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/state1");
+	$stateProvider
+		.state('state1', {
+			url: "/state1",
+			// templateUrl: "state1.html"
+			template: "<h1>hello</h1>",
+			// controller: someCtrl
+		})
+})
