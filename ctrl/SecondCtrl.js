8fd1e85e841ac1a7b9528dd05b28be904d6b0729
Kevin Kopp <koppks323@yahoo.com>
2015-11-24 20:51:21 -0700

    second state

---
+++ ctrl/SecondCtrl.js
@@ -0,0 +1,8 @@
+app.controller('SecondCtrl', function($scope) {
+	$scope.list1 = "There are many different methods to price retail products. The most commonly used are:";
+	$scope.list2 = "Cost-Based/Markup Pricing";
+	$scope.list3 = "Perhaps the most common and popular way to price products. Basically, this is a pricing method in which a fixed sum or a percentage of the total cost is added (as income or profit) to the cost of the product to arrive at its selling price.";
+	$scope.list4 = "Value-Based Pricing";
+	$scope.list5 = "This approach considers the value of its product or service to the customer, as opposed to the cost the company incurred to create and produce it. Successfully using this technique depends on the uniqueness of your product. If you have a highly unique product (such as the iPhone), you may be able to charge a higher price as people would be willing to pay more for it. However, if it is something that is more common different strategies need to be employed.";
+
+});
--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/");
 	$stateProvider
 		.state("state1", {
-			url: "/state1",
+			url: "/",
 			templateUrl: "partials/state1.html",
 			controller: "WmtCtrl"
+		})
+		.state("state2", {
+			url: "/state2",
+			templateUrl: "partials/state2.html",
+			controller: "SecondCtrl"
 		})
 })
 
